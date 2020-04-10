eca61b97245c3598e4744fd0f492167634a23d10
Daphne <daphne.feresin@gmail.com>
2020-04-10 18:58:21 -0400

    cors

--- main.js
+++ main.js
@@ -49,8 +49,7 @@ document.getElementById('cc-form').addEventListener('submit', function(e) {
   document.getElementById("reveal-btn").onclick = function(e) {
       e.preventDefault(); 
       console.log(encrypted)
-      const proxyurl = "https://cors-anywhere.herokuapp.com/";
-      fetch(proxyurl+'https://vgs-app.herokuapp.com/post', {
+      fetch('https://vgs-app.herokuapp.com/post', {
       method: 'POST', 
       headers: {
           'Content-Type': 'application/json',
