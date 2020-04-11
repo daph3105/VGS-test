cf001661e4e7afedb789e1cebdb07ea3600460e5
Daphne <daphne.feresin@gmail.com>
2020-04-11 00:49:59 -0400

    cors

--- app.js
+++ app.js
@@ -15,10 +15,16 @@ const tunnel = require('tunnel');
 app.use(bodyParser.json());
 
 
-app.use(cors({
-  credentials: true,
-  origin: ['*']
-}));
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if ('OPTIONS' == req.method) {
+  res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 
 
