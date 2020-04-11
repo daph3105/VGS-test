f67d22bf1bc7806dae8f7332084649d278668fc5
Daphne <daphne.feresin@gmail.com>
2020-04-11 12:21:06 -0400

    comments

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var cors = require('cors');
+
+//Dotenv middle-ware to load variables from .env file.
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+//VGS credentials from .env file
 const identifier =  process.env.IDENTIFIER;
 const user = process.env.USER;
 const pass = process.env.PASS
 
+//file system module to read certificate under path/to/cert.pem
 const fs = require('fs');
 const request = require('request');
 const tunnel = require('tunnel');
 
+//body-parser middleware to extract form input and expose it on req.body
 app.use(bodyParser.json());
 
-
+// to handle CORS policy
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -25,11 +31,13 @@ app.all('*', function(req, res, next) {
     next();
   }
 });
+//---------------------
 
 
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+//outbound/forward proxy assigned to tunnelingAgent
 const tunnelingAgent = tunnel.httpsOverHttp({
     ca: [ fs.readFileSync('path/to/cert.pem')],
     proxy: {
@@ -44,7 +52,7 @@ const tunnelingAgent = tunnel.httpsOverHttp({
 app.get('/', (req, res) => res.send('Outbound server'))
 
 
-
+//Sending outbound request to vgs echo server
 app.post('/post', function (req, res) {
 request({
     url: 'https://echo.apps.verygood.systems/post',
@@ -56,6 +64,7 @@ request({
     if(error) {
       console.log(error);
     } else {
+// if successfull, revealed data is returned
       console.log('Status:', response.statusCode);
       res.json(JSON.parse(body).json);
     }
